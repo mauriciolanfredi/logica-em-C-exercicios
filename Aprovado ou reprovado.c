70421aad581c7b6575377fa6bd81a6ed808c4931
Maurício Lanfredi <67252345+mauriciolanfredi@users.noreply.github.com>
2023-03-10 16:17:34 -0300

    Add files via upload

---
+++ Aprovado ou reprovado.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	float m;
+	
+	printf("Digite a nota\n");
+	scanf("%f", &m);
+	
+
+	if (m >= 7)
+	{
+		printf("Aprovado\n");
+	}
+	else
+	{ 
+		printf("Reprovado");
+	} 
+}
